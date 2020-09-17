58e9a6e71b22748be22e4863004c5d996fc457b3
Dean Alam <s1gamingc@gmail.com>
2020-09-17 14:33:37 -0400

    fixed Resolution

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
                 new Case("200R","ASUS","EVGA 600",
                 new Dimensions(16,9,20)),
         new Motherboard("LGA 1151","Gigabyte",4,3,"GB Bios"),
-        new Monitor(new Resolution(1080,1920),"Acer","CB272"));
+        new Monitor(new Resolution(1920,1080),"Acer","CB272"));
 
         DeansPC.description();
 
